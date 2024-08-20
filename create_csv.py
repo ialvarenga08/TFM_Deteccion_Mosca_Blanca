8194b55f86e5170b3c5e4834ffdfa92e8e749b21
ialvarenga08 <114862572+ialvarenga08@users.noreply.github.com>
2024-08-20 10:32:32 +0200

    Update create_csv.py

--- create_csv.py
+++ create_csv.py
@@ -1,6 +1,3 @@
-# Script to create CSV data file from Pascal VOC annotation files
-# Based off code from GitHub user datitran: https://github.com/datitran/raccoon_dataset/blob/master/xml_to_csv.py
-
 import os
 import glob
 import pandas as pd
@@ -12,25 +9,30 @@ def xml_to_csv(path):
         tree = ET.parse(xml_file)
         root = tree.getroot()
         for member in root.findall('object'):
-            value = (root.find('filename').text,
-                     int(root.find('size')[0].text),
-                     int(root.find('size')[1].text),
-                     member[0].text,
-                     int(member[4][0].text),
-                     int(member[4][1].text),
-                     int(member[4][2].text),
-                     int(member[4][3].text)
-                     )
+            filename = root.find('filename').text
+            width = int(root.find('size/width').text)
+            height = int(root.find('size/height').text)
+            object_class = member.find('name').text
+            
+            # Buscar directamente los elementos de 'bndbox'
+            bndbox = member.find('bndbox')
+            xmin = int(bndbox.find('xmin').text)
+            ymin = int(bndbox.find('ymin').text)
+            xmax = int(bndbox.find('xmax').text)
+            ymax = int(bndbox.find('ymax').text)
+            
+            value = (filename, width, height, object_class, xmin, ymin, xmax, ymax)
             xml_list.append(value)
+    
     column_name = ['filename', 'width', 'height', 'class', 'xmin', 'ymin', 'xmax', 'ymax']
     xml_df = pd.DataFrame(xml_list, columns=column_name)
     return xml_df
 
 def main():
-    for folder in ['train','validation']:
+    for folder in ['train', 'validation']:
         image_path = os.path.join(os.getcwd(), ('images/' + folder))
         xml_df = xml_to_csv(image_path)
         xml_df.to_csv(('images/' + folder + '_labels.csv'), index=None)
-        print('Successfully converted xml to csv.')
+        print(f'Successfully converted XML to CSV for {folder}.')
 
 main()
